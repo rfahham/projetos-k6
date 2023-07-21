9be31ad0c7acccef1ccccbf4dfa58ad769553ef0
Ricardo Fahham <rfahham@gmail.com>
2023-07-21 13:03:42 -0700

    Update globoplay.js

--- globoplay.js
+++ globoplay.js
@@ -2,27 +2,12 @@ import { sleep } from 'k6'
 import http from 'k6/http'
 
 export const options = {
-  ext: {
-    loadimpact: {
-      distribution: { 'amazon:br:sao paulo': { loadZone: 'amazon:br:sao paulo', percent: 100 } },
-      apm: [],
-    },
-  },
-  thresholds: {},
-  scenarios: {
-    Stages: {
-      executor: 'ramping-vus',
-      gracefulStop: '30s',
-      stages: [
-        { target: 1, duration: '10s' },
-        { target: 1, duration: '10s' },
-        { target: 0, duration: '10s' },
-      ],
-      gracefulRampDown: '30s',
-      exec: 'stages',
-    },
-  },
-}
+  stages: [
+    { duration: '10s', target: 10 },
+    { duration: '10s', target: 20 },
+    { duration: '10s', target: 0 },
+  ],
+};
 
 export function stages() {
   let response
