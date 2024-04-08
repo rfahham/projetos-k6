1ab57dac0bcdbc5053a76eb04661fae06ab6a75e
rfahham <rfahham@hotmail.com>
2024-04-08 17:55:14 -0300

    script random-status-code.js

--- executando-make/random-status-code.js
+++ executando-make/random-status-code.js
@@ -10,8 +10,11 @@ export let options = {
 };
 
 export default function () {
-    let res = http.get('http://random-status-code.herokuapp.com/');
+    let res = http.get('https://httpstat.us/Random/200,201,404,500-504');
 check(res, { 'status was 200': (r) => r.status == 200 });
+check(res, { 'status was 201': (r) => r.status == 201 });
+check(res, { 'status was 500': (r) => r.status == 500 });
+check(res, { 'status was 501': (r) => r.status == 501 });
 console.log(res.status)
   sleep(1);
 }
